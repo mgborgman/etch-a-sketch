42941002c65ef3f8201f8fad5f320fba747abeda
mgborgman <mgborgman#gmail.com>
2022-10-26 13:28:08 -0700

    JavaScript Etch-A-Sketch with options

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let gridSize = gridWidth**2;
 let count = 1;
 let cellSize = 960 / gridWidth;
 let gridSizeButton = document.getElementById('grid-size');
+let plainEtchASketchButton = document.getElementById('plain');
+let randomRGBEtchASketchButton = document.getElementById('randomRGB');
 
 gridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, ${cellSize}px)`;
     gridContainer.style.gridTemplateRows = `repeat(${gridWidth}, ${cellSize}px)`;
@@ -14,10 +16,48 @@ function createGrid(gridSize) {
         let gridElement = document.createElement('div');
         gridContainer.appendChild(gridElement);
         gridElement.addEventListener('mouseenter', function() {
-            this.classList.add('cell-shaded');
+            if(plainEtchASketchButton.classList.contains('clicked')) {
+                this.classList.add('cell-shaded');
+            } else if (randomRGBEtchASketchButton.classList.contains('clicked')) {
+                let randomR = getRandomInt(256);
+                let randomG = getRandomInt(256);
+                let randomB = getRandomInt(256);
+                this.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+            } else {
+                this.classList.add('cell-shaded');
+            }
+            
         });
     }
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+plainEtchASketchButton.addEventListener('click', function() {
+    if(this.classList.contains('clicked')) {
+        this.classList.remove('clicked');
+    } else {
+        this.classList.add('clicked')
+        randomRGBEtchASketchButton.classList.remove('clicked');
+    }
+    count = 1;
+    gridContainer.replaceChildren();
+    createGrid(gridSize);
+});
+
+randomRGBEtchASketchButton.addEventListener('click', function() {
+    if(this.classList.contains('clicked')) {
+        this.classList.remove('clicked');
+    } else {
+        this.classList.add('clicked')
+        plainEtchASketchButton.classList.remove('clicked');
+    }
+    count = 1;
+    gridContainer.replaceChildren();
+    createGrid(gridSize);
+});
 
 gridSizeButton.addEventListener('click', function() {
     count = 1;
